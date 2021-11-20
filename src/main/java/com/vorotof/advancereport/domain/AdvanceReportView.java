f899ced4473680e45cb6bf66c855234afbdd1bba
croacker@mail.ru <7Rv5699400>
2021-11-20 19:36:33 +0800

    #1

---
+++ src/main/java/com/vorotof/advancereport/domain/AdvanceReportView.java
@@ -0,0 +1,19 @@
+package com.vorotof.advancereport.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+
+/**
+ * Авансовый отчет, sql-view.
+ */
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Accessors(chain = true)
+//@Entity
+public class AdvanceReportView {
+}
